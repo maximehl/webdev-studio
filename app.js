21dd2e47a1380895275a179cb0c684e18cc49231
Maxime <maxime_hendrikse_liu@brown.edu>
2022-09-29 18:50:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const apiRequest = async () => {
   const BASE_URL = 'https://www.fruityvice.com/api/'
 
-  // This endpoint (https://www.fruityvice.com/doc/index.html#api-GET-getAll) returns a list of all the fruits and their info, feel free to play around with different endpoints!
+  // This endpoint (https://www.fruityvice.com/doc/index.html#api-GET-getAll)
+  // returns a list of all the fruits and their info, feel free to play around
+  // with different endpoints!
   const resourcePath = 'fruit/all'
 
   // Making a fetch request to an API endpoint
-  // Note: a fetch request is an asynchronous operation, and `await` tells the program to wait until the request has been completed before continuing
+  // Note: a fetch request is an asynchronous operation, and `await` tells the
+  // program to wait until the request has been completed before continuing
   const endpoint = BASE_URL + resourcePath;
   const response = await fetch(buildProxyEndpoint(endpoint), {
     method: "GET",
@@ -26,19 +29,35 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
-  // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
+  // TODO: Use either `map and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  const compareFruit = (fruitToCompare) => {
+    return fruitToCompare.nutritions.sugar > 15;
+  }
+
+  const filteredFruit = fruitsArray.filter(compareFruit);
+
+  console.log(filteredFruit);
+
   // TODO: Create a new HTML element to display your data
+  const fruitList = document.createElement("ul");
+  //document.append(fruitList);
+  filteredFruit.map((item) => {
+    const fruitListItem = document.createElement("li");
+    fruitListItem.innerHTML = item.name;
+    fruitList.appendChild(fruitListItem);
+  })
 
   // TODO: Append your new element to the page
+  document.getElementById("cs1300-gallery").appendChild(fruitList);
 
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
-const exampleAddElement = () => {
+/*const exampleAddElement = () => {
   // Create a new HTML element and set its properties
   const newElement = document.createElement('div');
   newElement.innerHTML = "this text is inside a div";
@@ -46,7 +65,7 @@ const exampleAddElement = () => {
   // Append the new element to an existing part of the webpage
   const existingElement = document.getElementById('example-id');
   existingElement.append(newElement);
-}
+}*/
 
 /**
  * To access information in this API, we need to send our requests through a proxy due to CORS restrictions.
